f1c1cfcab9f409fec3aa6269638ee8da5da1d904
Phillip Whelan <pwhelan@exis.cl>
2020-07-07 10:16:10 -0400

    Update export help.

--- cmd/export.go
+++ cmd/export.go
@@ -51,13 +51,12 @@ type Config struct {
 // exportCmd represents the export command
 var exportCmd = &cobra.Command{
 	Use:   "export",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "export a machine configuration",
+	Long: `export the machine configuration to a PEM file. this configuration can be imported later with the import command.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+for example:
+    bash$ dm export MACHINE_NAME > MACHINE_NAME.pem
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 		configPath := configdir.LocalConfig("dm")
 		configPathMachines := filepath.Join(configPath, "machines")
